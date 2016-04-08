d0a35dc302b7bb7cda64359817cf0f065580b5ce
Fabricio Catae <fabricio.catae@microsoft.com>
2016-04-08 19:26:53 -0300

    TaskList Component

--- components.tsx
+++ components.tsx
@@ -10,6 +10,24 @@ var TaskComponent = React.createClass({
     }
 });
 
-ReactDOM.render(<TaskComponent name="react task 1"></TaskComponent>,
-    document.getElementById('container');
+var TaskListComponent = React.createClass({
+    render: function() {
+        var tasks = this.props.tasks;
+        
+        var list = tasks.map( (t) => {
+           return <TaskComponent name={t.name}></TaskComponent>; 
+        });
+        
+        return <div>{list}</div>;
+    }
+})
+
+var _taskList = [ 
+    { name: 'react task 1' },
+    { name: 'react task 2' },
+    { name: 'react task 3' },
+    ];
+
+ReactDOM.render(<TaskListComponent tasks={_taskList}></TaskListComponent>,
+    document.getElementById('container')
 )
