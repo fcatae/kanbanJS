830e936bd793df839d9079c4cac0703d3cded3f6
Fabricio Catae <fabricio.catae@microsoft.com>
2016-04-08 19:36:38 -0300

    replaced static HTML logic with React

--- components.tsx
+++ components.tsx
@@ -4,9 +4,12 @@ var ReactDOM;
 var TaskComponent = React.createClass({
     render: function() {
         return <div className="task">
-            <input type="checkbox" onchange="update()"></input>
+            <input type="checkbox" onChange={this.handleChange}></input>
             <input value={this.props.name}></input>
         </div>   
+    },
+    handleChange: function() {
+        alert('change');
     }
 });
 
@@ -22,12 +25,22 @@ var TaskListComponent = React.createClass({
     }
 })
 
-var _taskList = [ 
+var _activeTaskList = [ 
     { name: 'react task 1' },
     { name: 'react task 2' },
     { name: 'react task 3' },
     ];
 
-ReactDOM.render(<TaskListComponent tasks={_taskList}></TaskListComponent>,
-    document.getElementById('container')
-)
+var _doneTaskList = [ 
+    { name: 'old task A' },
+    { name: 'old task B' },
+    { name: 'old task C' },
+    ];
+
+ReactDOM.render(<TaskListComponent tasks={_activeTaskList}></TaskListComponent>,
+    document.querySelector('.task-list-active')
+);
+
+ReactDOM.render(<TaskListComponent tasks={_doneTaskList}></TaskListComponent>,
+    document.querySelector('.task-list-done')
+);
