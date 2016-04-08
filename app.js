3984407c1d9d64b73b801b71e7b4a4dca82bdf6e
Fabricio Catae <fabricio.catae@microsoft.com>
2016-04-08 18:50:32 -0300

    definindo a API

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ app.get('/', function(req,res) {
     res.end('hello world');    
 });
 
+// create task
+app.get('/create', function(req,res) {    
+    res.end('create');    
+});
+
+// update task
+app.get('/update', function(req,res) {    
+    res.end('update');    
+});
+
+// get task list
+app.get('/tasklist', function(req,res) {    
+    res.end('tasklist');    
+});
+
 app.listen(8080);
